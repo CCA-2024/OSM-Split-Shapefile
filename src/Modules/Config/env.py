d8eaba77d3e7f78a8ff55fa54641ec866dce0e0c
DevPyPedro <dev.santiago.felicio@gmail.com>
2025-01-09 17:51:12 -0300

    Update compose: create volumes protobufs

--- src/Modules/Config/env.py
+++ src/Modules/Config/env.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 configRun       = {
-    "SIZELIMIT":    eval(os.getenv("SIZELIMIT")),
+    "SIZELIMIT":    eval(str(os.getenv("SIZELIMIT"))),
     "PATHSAVE":     os.getenv("PATHSAVE"),
     "TYPE_MAP_OSM": os.getenv("TYPE_MAP_OSM"),
 }
